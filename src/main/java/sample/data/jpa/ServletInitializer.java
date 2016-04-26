d389cf37f03a038a296ec4d0593dbd7182f3d6c9
duyunchang <duyunchang@tv189.com>
2016-04-26 22:12:27 +0800

    xxx

--- src/main/java/sample/data/jpa/ServletInitializer.java
+++ src/main/java/sample/data/jpa/ServletInitializer.java
@@ -1,10 +1,13 @@
 package sample.data.jpa;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RestController;
 
 
-
+@SpringBootApplication
+@RestController
 public class ServletInitializer extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
